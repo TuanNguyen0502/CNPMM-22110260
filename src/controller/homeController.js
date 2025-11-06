3e35299096e75ee854db642c7fa5e4deb4580bee
Tuan <nguyenhahongtuan@gmail.com>
2025-11-06 14:51:01 +0700

    feat: implement homeController with CRUD operations and update web routes

---
+++ src/controller/homeController.js
@@ -0,0 +1,75 @@
+import db from "../models/index";
+import CRUDService from "../services/CRUDService";
+
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homePage.ejs", {
+      data: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+let getAboutPage = (req, res) => {
+  return res.render("test/about.ejs");
+};
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let getFindAllCrud = async (req, res) => {
+  let data = await CRUDService.getAllUsers();
+  return res.render("users/findAllUsers.ejs", {
+    data: JSON.stringify(data),
+  });
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
+
+let getEditCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    let userData = await CRUDService.getUserInfoById(userId);
+    return res.render("users/editUser.ejs", {
+      data: JSON.stringify(userData),
+    });
+  } else {
+    return res.send("User not found!");
+  }
+};
+
+let putCRUD = async (req, res) => {
+  let data = req.body;
+  let allUsers = await CRUDService.updateUserData(data);
+  return res.render("users/findAllUsers.ejs", {
+    dataList: JSON.stringify(allUsers),
+  });
+};
+
+let deleteCRUD = async (req, res) => {
+  let userId = req.query.id;
+  if (userId) {
+    await CRUDService.deleteUserById(userId);
+    return res.send("Delete user successfully!");
+  } else {
+    return res.send("User not found!");
+  }
+};
+
+module.exports = {
+  getHomePage,
+  getAboutPage,
+  getCRUD,
+  postCRUD,
+  getFindAllCrud,
+  getEditCRUD,
+  putCRUD,
+  deleteCRUD,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,5 @@
 import express from "express";
-import homeController from "../controllers/homeController";
+import homeController from "../controller/homeController";
 
 let router = express.Router(); // Use express router
 
@@ -9,7 +9,7 @@ let initWebRoutes = (app) => {
   router.get("/about", homeController.getAboutPage);
   router.get("/crud", homeController.getCRUD);
   router.post("/post-crud", homeController.postCRUD);
-  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/get-crud", homeController.getFindAllCrud);
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
